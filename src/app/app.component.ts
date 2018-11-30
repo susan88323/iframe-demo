30c85265a792942b794a6668ee6754a7da2287d7
Susan Wang <susanw@dev6.com>
2018-11-30 11:52:40 -0500

    Added message receiver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+  showFeatureTeacher = false;
+  receivedTime: any;
+  receiver: any;
+  message: any;
+
+  constructor() {}
+
+  ngOnInit() {
+  	this.receiver = (message) => {
+  		console.log(message);
+ 			this.message = message;
+	    let trusteddomain = "http://localhost:4200";
+
+	    var currenttime = new Date();
+	    this.receivedTime = currenttime.getHours() + ":" + currenttime.getMinutes() + ":" + currenttime.getSeconds();
+	    
+	    if (message.origin == trusteddomain) {
+	        const messageData = message.data;
+	        if (messageData === 'show feature teacher!') {
+	        	this.showFeatureTeacher = true;
+	        }
+	    }
+		}
+
+  	window.addEventListener('message', this.receiver, false);
+  }
+
+	ngOnDestroy() {
+    window.removeEventListener('message', this.receiver, false);
+  }
+
 }
